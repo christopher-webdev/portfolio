5189a148d8eb95d23b752adf3f7cb28c81edb778
christopher_webdev <christopher_webdev@outlook.com>
2025-02-22 01:47:59 +0100

    hh

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-<<<<<<< HEAD
   base: '/portfolio/',  // Ensure this matches your GitHub Pages repo name
   plugins: [react()]
 })
-=======
-  plugins: [react()],
-   
- 
-});
->>>>>>> 167b158e57d3aa28c38ebe4db85b0157a2661249
